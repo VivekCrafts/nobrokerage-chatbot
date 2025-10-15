7a65725d2de8d7d09ef082bf59dbc091aa1e5e0d
Vivek Machindra Vanave <vivekvanave45@gmail.com>
2025-10-15 12:29:23 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage.jsx";
 import ProjectDetails from "./pages/ProjectDetails";
-import TestProjectLinks from "./pages/testpageslink.jsx";
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ChatPage />} />
         <Route path="/project/:slug" element={<ProjectDetails />} />
-        <Route path="/test-links" element={<TestProjectLinks />} />
+        
       </Routes>
     </Router>
   );
